2ef5882dce1c78cdeabe2e1eba103967a6c2b1ea
cxdemxn <cxdemxn@gmail.com>
2025-10-28 22:02:38 +0100

    implemented base entity and repository

---
+++ src/main/java/com/chatr/shared/model/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.chatr.shared.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter @Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    protected Boolean deleted = false;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}

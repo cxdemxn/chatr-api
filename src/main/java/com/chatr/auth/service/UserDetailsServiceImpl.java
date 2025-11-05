1d15c8deccd7070bde186f356b9ca0cda55ce116
cxdemxn <cxdemxn@gmail.com>
2025-11-05 20:55:09 +0100

    implemented user details service

---
+++ src/main/java/com/chatr/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.chatr.auth.service;
+
+import com.chatr.user.model.User;
+import com.chatr.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return new UserDetailsImpl(user);
+    }
+}
--- src/main/java/com/chatr/user/repository/UserRepository.java
+++ src/main/java/com/chatr/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.chatr.user.repository;
 import com.chatr.shared.repository.BaseRepository;
 import com.chatr.user.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends BaseRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }

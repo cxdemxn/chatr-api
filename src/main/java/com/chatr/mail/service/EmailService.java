a29149b921bce6a830ca22b127a6f499d8b35a6d
cxdemxn <cxdemxn@gmail.com>
2025-11-20 06:52:59 +0100

    fixed issue with verify-sign-up-code endpoint sending verification codes to existing emails

--- src/main/java/com/chatr/mail/service/EmailService.java
+++ src/main/java/com/chatr/mail/service/EmailService.java
@@ -1,8 +1,10 @@
 package com.chatr.mail.service;
 
+import com.chatr.shared.exceptions.UserAlreadyExistsException;
 import com.chatr.shared.mail.EmailVerificationServiceImpl;
 import com.chatr.shared.mail.Emailer;
 import com.chatr.shared.utils.EmailUtil;
+import com.chatr.user.repository.UserRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,13 @@ public class EmailService {
     private final EmailUtil emailUtil;
     private final Emailer emailer;
     private final EmailVerificationServiceImpl emailVerificationService;
+    private final UserRepository userRepository;
 
     public void sendSignUpCode(@NotNull String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new UserAlreadyExistsException("Email already in use");
+        }
+
         String code = emailUtil.generateSignUpCode();
         emailVerificationService.cacheVerificationCode(email, code);
         emailer.sendPlainText(email, "noreply@chatr.dev", "Email Verification", "registration code is: " + code);
